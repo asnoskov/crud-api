f0a3dd2d389603011fbc2b747ef3a3258aa8bf6e
Aleksei Noskov <aleksei_noskov@epam.com>
2023-01-11 22:00:42 +0100

    simple in-memory database implementation

---
+++ src/entities/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    constructor (public id: string, public userName: string, public age: number, public hobbies: string[]){};
+
+    public clone(): User {
+        return new User(this.id, this.userName, this.age, [...this.hobbies]);
+    }
+}
